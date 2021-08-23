2e898bae833c9a56db9f3e39a8be04649a061ef8
Zeeshan Shaikh <zeeshanshaikh1205@gmail.com>
2021-08-22 21:46:17 -0400

    Alternate path algo added

--- Prototype/alternatePath.py
+++ Prototype/alternatePath.py
@@ -211,7 +211,7 @@ class RRT:
                 self.node_list.append(new_node)
 
             if animation and i % 2 == 0:
-                self.draw_graph(rnd_node, z, z2)
+                self.draw_graph(rnd_node)
 
             # Check if the distance from the latest node to the goal state is within the set area
             if self.calc_dist_to_goal(self.node_list[-1].x, self.node_list[-1].y) <= self.expand_dis:
@@ -223,7 +223,7 @@ class RRT:
 
             if animation and i % 2:
                 done = True
-                self.draw_graph(rnd_node, z, z2)
+                self.draw_graph(rnd_node, z)
 
         return None  # cannot find path
 
@@ -382,169 +382,11 @@ class RRT:
         theta = math.atan2(dy, dx)
         return d, theta
 
-# ------------------------------- Pure Pursuit Controller here ---------------------------------------------------------
-
-# Parameters
-k = 0.05  # look forward gain
-Lfc = 2.5  # [m] look-ahead distance
-Kp = 1.0  # speed proportional gain
-dt = 0.1  # [s] time tick
-WB = 2.9  # [m] wheel base of vehicle
-
-show_animation = True
-
-
-class State:
-
-    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
-        self.x = x
-        self.y = y
-        self.yaw = yaw
-        self.v = v
-        self.rear_x = self.x - ((WB / 2) * math.cos(self.yaw))
-        self.rear_y = self.y - ((WB / 2) * math.sin(self.yaw))
-
-    def update(self, a, delta):
-        self.x += self.v * math.cos(self.yaw) * dt
-        self.y += self.v * math.sin(self.yaw) * dt
-        self.yaw += self.v / WB * math.tan(delta) * dt
-        self.v += a * dt
-        self.rear_x = self.x - ((WB / 2) * math.cos(self.yaw))
-        self.rear_y = self.y - ((WB / 2) * math.sin(self.yaw))
-
-    def calc_distance(self, point_x, point_y):
-        dx = self.rear_x - point_x
-        dy = self.rear_y - point_y
-        return math.hypot(dx, dy)
-
-
-class States:
-
-    def __init__(self):
-        self.x = []
-        self.y = []
-        self.yaw = []
-        self.v = []
-        self.t = []
-
-    def append(self, t, state):
-        self.x.append(state.x)
-        self.y.append(state.y)
-        self.yaw.append(state.yaw)
-        self.v.append(state.v)
-        self.t.append(t)
-
-
-def proportional_control(target, current):
-    a = Kp * (target - current)
-
-    return a
-
-
-class TargetCourse:
-
-    def __init__(self, cx, cy):
-        self.cx = cx
-        self.cy = cy
-        self.old_nearest_point_index = None
-
-    def search_target_index(self, state):
-
-        # To speed up nearest point search, doing it at only first time.
-        if self.old_nearest_point_index is None:
-            # search nearest point index
-            dx = [state.rear_x - icx for icx in self.cx]
-            dy = [state.rear_y - icy for icy in self.cy]
-            d = np.hypot(dx, dy)
-            ind = np.argmin(d)
-            self.old_nearest_point_index = ind
-        else:
-            ind = self.old_nearest_point_index
-            distance_this_index = state.calc_distance(self.cx[ind],
-                                                      self.cy[ind])
-            while True:
-                distance_next_index = state.calc_distance(self.cx[ind + 1],
-                                                          self.cy[ind + 1])
-                if distance_this_index < distance_next_index:
-                    break
-                ind = ind + 1 if (ind + 1) < len(self.cx) else ind
-                distance_this_index = distance_next_index
-            self.old_nearest_point_index = ind
-
-        Lf = k * state.v + Lfc  # update look ahead distance
-
-        # search look ahead target point index
-        while Lf > state.calc_distance(self.cx[ind], self.cy[ind]):
-            if (ind + 1) >= len(self.cx):
-                break  # not exceed goal
-            ind += 1
-
-        return ind, Lf
-
-
-def pure_pursuit_steer_control(state, trajectory, pind):
-    ind, Lf = trajectory.search_target_index(state)
-
-    if pind >= ind:
-        ind = pind
-
-    if ind < len(trajectory.cx):
-        tx = trajectory.cx[ind]
-        ty = trajectory.cy[ind]
-    else:  # toward goal
-        tx = trajectory.cx[-1]
-        ty = trajectory.cy[-1]
-        ind = len(trajectory.cx) - 1
-
-    alpha = math.atan2(ty - state.rear_y, tx - state.rear_x) - state.yaw
-
-    delta = math.atan2(2.0 * WB * math.sin(alpha) / Lf, 1.0)
-
-    return delta, ind
-
-
-def draw_graph2(self, rnd=None, zono=None, zono2=None):
-    plt.clf()
-    if rnd is not None:
-        plt.plot(rnd.x, rnd.y, "^k")
-    for node in self.node_list:
-        if node.parent:
-            plt.plot(node.path_x, node.path_y, "-g")
-
-    for (ox, oy, size) in self.obstacle_list:
-        self.plot_circle(ox, oy, size)
-
-    if zono is not None:
-        self.plot_circle1(zono)
-
-    if zono2 is not None:
-        self.plot_circle1(zono2)
-
-    plt.plot(self.start.x, self.start.y, "xr")
-    plt.plot(self.end.x, self.end.y, "xr")
-    plt.axis("equal")
-    plt.axis([-7, 20, -7, 20])
-    plt.grid(True)
-    plt.pause(0.01)
-
-
-def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
-    """
-    Plot arrow
-    """
-
-    if not isinstance(x, float):
-        for ix, iy, iyaw in zip(x, y, yaw):
-            plot_arrow(ix, iy, iyaw)
-    else:
-        plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
-                  fc=fc, ec=ec, head_width=width, head_length=width)
-        plt.plot(x, y)
 
 def main(gx=6.0, gy=10.0):
+    # Variable declarations here
+    all_paths = []
     print("start " + __file__)
-    # Declare required variables here
-    follow = []
 
     # ====Search Path with RRT====
     obstacleList = [(5, 5, 1), (3, 6, 2), (3, 8, 2), (3, 10, 2), (7, 5, 2),
@@ -556,103 +398,30 @@ def main(gx=6.0, gy=10.0):
         obstacle_list=obstacleList)
     path = rrt.planning(animation=show_animation)
 
-    # The 'path' is in reversed order i.e the starting point is at the end of the list "path"
-    # We reverse the order here
-    list_length = len(path)
-    high = list_length - 1
-    while(high >= 0):
-        follow.append(path[high])
-        high -= 1
-
-
     if path is None:
         print("Cannot find path")
     else:
-        print("Path found!")
+        print("found path!!")
 
-        if show_animation:
-            # Draw final path
-            if show_animation:
-                rrt.draw_graph()
-                plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
-                plt.grid(True)
-                plt.pause(0.01)  # Need for Mac
-                plt.show()
-
-
-    # Motion planning starts here
-
-    goal = follow
-    goal_x = []
-    goal_y = []
-
-    for i in range(len(goal)):
-        goal_x.append(goal[i][0])
-        goal_y.append(goal[i][1])
-
-    #  target course
-    cx = goal_x
-    cy = goal_y
-
-    target_speed = 10.0 / 20  # [m/s]
-
-    T = 100.0  # max simulation time
-
-    # initial state
-    state = State(x=goal_x[0], y=goal_y[0], yaw=0.0, v=0.0)
-
-    lastIndex = len(cx) - 1
-    time = 0.0
-    states = States()
-    states.append(time, state)
-    target_course = TargetCourse(cx, cy)
-    target_ind, _ = target_course.search_target_index(state)
-
-    while T >= time and lastIndex > target_ind:
-
-        # Calc control input
-        ai = proportional_control(target_speed, state.v)
-        di, target_ind = pure_pursuit_steer_control(
-            state, target_course, target_ind)
-
-        state.update(ai, di)  # Control vehicle
-
-        time += dt
-        states.append(time, state)
-
-        if show_animation:  # pragma: no cover
-            plt.cla()
-            for (ox, oy, size) in obstacleList:
-                deg = list(range(0, 360, 5))
-                deg.append(0)
-                xl = [ox + size * math.cos(np.deg2rad(d)) for d in deg]
-                yl = [oy + size * math.sin(np.deg2rad(d)) for d in deg]
-                plt.plot(xl, yl, "b-")
-
-            plot_arrow(state.x, state.y, state.yaw)
-            plt.plot(cx, cy, "-r", label="course")
-            plt.plot(states.x, states.y, "-g", label="trajectory")
-            plt.plot(cx[target_ind], cy[target_ind], "xg", label="target")
-            plt.axis("equal")
-            plt.grid(True)
-            plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
-            plt.pause(0.001)
-
-
-    # Test
-    assert lastIndex >= target_ind, "Cannot goal"
-    plt.plot(cx, cy, ".r", label="course")
-    plt.plot(states.x, states.y, "-b", label="trajectory")
-    plt.grid(True)
-    plt.pause(0.01)
-    plt.show()
-
-
-    plt.plot(states.t, [iv * 3.6 for iv in states.v], "-r")
-    plt.xlabel("Time[s]")
-    plt.ylabel("Speed[km/h]")
-    plt.grid(True)
-    plt.show()
+    for every_node in path:
+        temp_x = every_node[0]
+        temp_y = every_node[1]
+        rrt = RRT(start=[temp_x, temp_y],
+                  goal=[gx, gy],
+                  rand_area=[-2, 15],
+                  obstacle_list=obstacleList)
+        new_path = rrt.planning(animation=show_animation)
+        all_paths.append(new_path)
+    # Draw final paths
+    if show_animation:
+        rrt.draw_graph()
+        plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
+        for every_path in all_paths:
+            plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
+            plt.plot([x for (x, y) in every_path], [y for (x, y) in every_path], '-g')
+        plt.grid(True)
+        plt.pause(0.01)  # Need for Mac
+        plt.show()
 
 
 if __name__ == '__main__':
